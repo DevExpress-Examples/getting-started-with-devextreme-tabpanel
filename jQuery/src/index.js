e888a5da2bf6f86a2fc07213a6a47003fc54da0e
Alexander Yakovlev <alexander.yakovlev@devexpress.com>
2021-05-20 19:09:24 +0300

    Fix quotes

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -41,10 +41,10 @@ $(function(){
 });
 
 const employeeData = {
-    name: 'John Heart',
-    position: 'CEO',
+    name: "John Heart",
+    position: "CEO",
     hireDate: new Date(2012, 4, 13),
     officeNumber: 901,
-    notes: 'John has been in the Audio/Video industry since 1990. He has led DevAV as its CEO since 2003.',
+    notes: "John has been in the Audio/Video industry since 1990. He has led DevAV as its CEO since 2003.",
     roles: ["Chief Officer", "Manager", "Administrator"]
 };
