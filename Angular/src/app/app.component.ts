09ecb61c05ffec5eb401d4f7a52760d37def87cf
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:06:10 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { DxRadioGroupTypes } from 'devextreme-angular/ui/radio-group';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   employeeData = {
